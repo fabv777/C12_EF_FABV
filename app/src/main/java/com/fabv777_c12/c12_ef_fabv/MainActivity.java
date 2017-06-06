c9e4b58e6933048e02405fa9ba3057241bab9509
Fabián <fabv777@gmail.com>
2017-06-06 13:34:40 -0400

    Cambios de último momento.

--- app/src/main/java/com/fabv777_c12/c12_ef_fabv/MainActivity.java
+++ app/src/main/java/com/fabv777_c12/c12_ef_fabv/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 
